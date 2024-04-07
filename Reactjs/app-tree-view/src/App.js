b292341f8c68afea4007efebb7f91a347f60b7bf
chandrakala918 <chandrakaladande918@gmail.com>
2024-04-07 15:07:30 +0530

    Added

--- Reactjs/app-tree-view/src/App.js
+++ Reactjs/app-tree-view/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import TreeView from './Components';
+import menus from './Components/data';
+
 
 function App() {
   return (
     <div className="App">
-    <TreeView menus={menus} />
+   Tree view component/menu UI component / recursive navigation menu
+     <TreeView menus={menus} />
+
     </div>
   );
 }
