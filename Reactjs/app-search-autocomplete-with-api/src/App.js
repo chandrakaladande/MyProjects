18ecf43b365575fe2a3e55ccfedfb7706de15100
chandrakala918 <chandrakaladande918@gmail.com>
2024-04-07 16:16:07 +0530

    Added

--- Reactjs/app-search-autocomplete-with-api/src/App.js
+++ Reactjs/app-search-autocomplete-with-api/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import SearchAutocomplete from './Components/SearchAutocomplete';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Search Autocomplete
+       <SearchAutocomplete/>
     </div>
   );
 }
