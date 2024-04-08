8adb72ce678fb36d395e19cab71f5c57f791e37a
chandrakala918 <chandrakaladande918@gmail.com>
2024-04-08 10:50:39 +0530

    Added

--- Reactjs/app-use-fetch/src/App.js
+++ Reactjs/app-use-fetch/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import UseFetchHookTest from './Test';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+       useFetch - Custom Hook Test
+       <UseFetchHookTest/>
     </div>
   );
 }
