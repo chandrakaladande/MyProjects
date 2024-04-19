100b556b2dba3d2a124ae65eae48b5de5aab7a3d
chandrakala918 <chandrakaladande918@gmail.com>
2024-04-19 19:37:54 +0530

    Added

--- Javascript/SideBar/app.js
+++ Javascript/SideBar/app.js
@@ -1,15 +1,16 @@
-// select modal-btn,modal-overlay,close-btn
-// listen for click events on modal-btn and close-btn
-// when user clicks modal-btn add .open-modal to modal-overlay
-// when user clicks close-btn remove .open-modal from modal-overlay
-
-const modalBtn = document.querySelector(".modal-btn");
-const modal = document.querySelector(".modal-overlay");
+const toggleBtn = document.querySelector(".sidebar-toggle");
 const closeBtn = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
 
-modalBtn.addEventListener("click", function () {
-  modal.classList.add("open-modal");
+toggleBtn.addEventListener("click", function () {
+  // if (sidebar.classList.contains("show-sidebar")) {
+  //   sidebar.classList.remove("show-sidebar");
+  // } else {
+  //   sidebar.classList.add("show-sidebar");
+  // }
+  sidebar.classList.toggle("show-sidebar");
 });
+
 closeBtn.addEventListener("click", function () {
-  modal.classList.remove("open-modal");
+  sidebar.classList.remove("show-sidebar");
 });
